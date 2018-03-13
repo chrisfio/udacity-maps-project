7adb1cb32cdb9d531ff288beafbe5d73e688565d
Chris Fiorino <chrisfiorino@gmail.com>
2018-03-13 08:18:30 -0700

    Quick typo adjustment

--- js/app-gmaps.js
+++ js/app-gmaps.js
@@ -144,7 +144,6 @@ function hideListings() {
 	for(var i = 0; i < markers.length; i++){
 		markers[i].setMap(null); 
 	}
-
 	my.viewModel.mapList().forEach(function(establishment){ 
 		establishment.show = false;
 	});
